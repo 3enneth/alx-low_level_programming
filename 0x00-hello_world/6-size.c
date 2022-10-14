f1a119f0ba11687a0f13636c836c1ac825d5dad3
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-13 21:05:25 -0700

    Size six

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -is the entry point for the program.
+ *
+ * Return:zero if no error, Non-zero value if errors.
+ */
+int main(void)
+{
+printf("Size of a char: %Id byte(s)\n", sizeof(char));
+printf("Size of an int: %Id byte(S)\n", sizeof(int));
+printf("Size of a long int: %Id byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %Id byte(s)\n", sizeof(long long));
+printf("Size of a float: %Id byte(S)\n", sizeof(float));
+}
