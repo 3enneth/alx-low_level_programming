858e6f50c3617b9dec8785f05ce5d3463c1db453
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-13 19:37:15 -0700

    C puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+include <stdio.h>
+/**
+ *main- Serves as the entry point to the program
+ *
+ *Return: By default returns zero if no errors otherwise returns non-zero value.*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); return (0)}
