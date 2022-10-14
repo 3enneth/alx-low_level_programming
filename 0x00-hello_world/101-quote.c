c2bd5e607e914b3f1dafd211b116d67a77a40f01
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-13 21:27:59 -0700

    Quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-program entry point.
+ *
+ * Return: 1 on success.
+ */
+int main(void)
+{
+write(2, "and that place of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
