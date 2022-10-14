bfb1ab7167160a472345ff137b351d4954f05f6f
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-13 20:26:03 -0700

    F print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:o if no errors, return non zero if errors
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,/n");
+return (0);
+}
