cd5fceedab522a0b01ee59a5e7d87878e22d1095
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-13 21:34:08 -0700

    Quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(2, "and that place of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+fwrite(2, "and that place of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
