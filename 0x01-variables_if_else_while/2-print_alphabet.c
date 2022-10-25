5a61363c1bc3aa7646d182331ec96f1a65d4706a
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-24 18:07:49 -0700

    Alphabet print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,15 @@
 
 /**
  *main-program entry point.
- *Return:0 no error, non zero if error.
+ *Return:0 if no error, non zero if error.
  **/
 int main(void)
 {
 	char i;
-	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
-	}
-	for (j = 'A'; j <= 'Z'; j++)
-	{
-		putchar(j);
 	}
 	putchar('\n');
 	return (0);
