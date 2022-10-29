1edacff43b0bf471ce4c2ad72a82f43fd4e95798
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-29 05:55:42 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: result of addition
+ */
+
+int add(int x, int y)
+
+{
+	int solve = (x + y);
+
+	return (solve);
+}
