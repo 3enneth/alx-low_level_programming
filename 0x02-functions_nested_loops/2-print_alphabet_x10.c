53f549e9dbb6752786671e4d84aeb9060e6ef9a6
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-28 19:34:31 -0700

    Task 2b

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_10 - prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
