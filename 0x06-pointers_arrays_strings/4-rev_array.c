7a2546827988809aa28a5614a6aecb1430489d48
Kenneth Onu <kennethonumadu@gmail.com>
2022-10-29 14:55:49 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,63 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- *
- * Return: void
- */
-
-void print_line(char *c, int s, int l)
-
-{
-	int j, k
-		;
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers
+ * @n: size of  array
  */
 
-void print_buffer(char *b, int size)
+void reverse_array(int *a, int n)
 
 {
 	int i;
+	int tmp;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	for (i = 0; i < (n / 2); i++)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
 	}
-	if (size == 0)
-		putchar('\n');
 }
